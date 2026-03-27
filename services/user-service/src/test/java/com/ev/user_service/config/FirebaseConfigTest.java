9b33776e431adf2e9529a6bf0b42a48c8cc93659
phu-boop <phudz25022005@gmail.com>
2026-03-27 19:07:26 +0700

    add: unitest for all service

--- services/user-service/src/test/java/com/ev/user_service/config/FirebaseConfigTest.java
+++ services/user-service/src/test/java/com/ev/user_service/config/FirebaseConfigTest.java
@@ -3,14 +3,18 @@ package com.ev.user_service.config;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.io.IOException;
+import org.springframework.core.io.Resource;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class FirebaseConfigTest {
+
+    @Mock
+    private Resource firebaseKey;
 
     @InjectMocks
     private FirebaseConfig firebaseConfig;
@@ -21,6 +25,9 @@ class FirebaseConfigTest {
         // firebase-service-account.json might not exist,
         // the method should gracefully handle the missing file by just printing a
         // warning and returning.
+
+        when(firebaseKey.exists()).thenReturn(false);
+        when(firebaseKey.getDescription()).thenReturn("class path resource [mock-firebase.json]");
 
         assertDoesNotThrow(() -> firebaseConfig.initFirebase());
     }
